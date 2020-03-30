4b3b8a53b11271268ed139daed9f4bb963497a3c
Ruslan Plaksunov <ruslan.plaxunov@yandex.ru>
2020-03-30 14:18:57 +0300

    Frontend version 0.3 with react layout: login,logout,user_list

---
+++ frontend/static/js/components/LogIn.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./comp_style/style.scss";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { login: "", password: "" };
+
+    this.onChangeLogin = this.onChangeLogin.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    let user = {
+      username: this.state.login,
+      password: this.state.password
+    };
+    let response = fetch("http://127.0.0.1:8000/auth/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      body: JSON.stringify(user)
+    });
+    if (response) alert(`${this.state.login}, добро пожаловать! ` + response);
+    event.preventDefault();
+  }
+
+  onChangePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  onChangeLogin(event) {
+    this.setState({ login: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <p>
+          <label>
+            {" "}
+            Логин:{" "}
+            <input
+              type="text"
+              name="login"
+              value={this.state.login}
+              onChange={this.onChangeLogin}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            {" "}
+            Пароль:{" "}
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+            />
+          </label>
+        </p>
+        <p>
+          <input type="submit" value="Submit" />
+        </p>
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
---
+++ frontend/static/js/components/LogOut.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./comp_style/style.scss";
+
+class LogOutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { permission: "19292b0fa328f8bacd9e166f535177c99be151b9" };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    let user = {
+      Token: this.state.permission
+    };
+    let response = fetch("http://127.0.0.1:8000/auth/logout/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: "Token " + this.state.permission
+      },
+      body: JSON.stringify(user)
+    });
+    if (response) alert(`${this.state.login}, Chao kakao !` + response.body);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <p>
+          <input type="submit" value="LogOut" />
+        </p>
+      </form>
+    );
+  }
+}
+
+export default LogOutForm;
