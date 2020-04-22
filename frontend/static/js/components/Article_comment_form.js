7d6497ee50d02a054c79c275029b866251dcdb8e
Ruslan Plaksunov <ruslan.plaxunov@yandex.ru>
2020-04-22 18:32:07 +0300

    comment form

--- frontend/static/js/components/Article_comment_form.js
+++ frontend/static/js/components/Article_comment_form.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter, Link } from "react-router-dom";
-import "./comp_style/article_retrieve.scss";
+import "./comp_style/article_comment_form.scss";
 
 class Article_comment_form extends React.Component {
   state = {
@@ -9,10 +9,10 @@ class Article_comment_form extends React.Component {
     creator: [],
   };
   onSubmit(event) {
-    var description = document.getElementById("description").value;
+    var comments = document.getElementById("comments").value;
     let comment = {
-      description: description,
-      id_post: this.props.id_post,
+      description: comments,
+      id_post: 1,
     };
     let response = fetch("http://127.0.0.1:8000/notes/", {
       method: "POST",
@@ -27,11 +27,16 @@ class Article_comment_form extends React.Component {
   render() {
     const { comments, creator } = this.state;
     return (
-      <div class="commetnt">
+      <div class="commetnt_form">
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
-            <label for="description">Leave your comment here ...</label>
-            <input id="description" type="text" />
+            <textarea
+              name="comments"
+              id="comments"
+              cols="90"
+              rows="10"
+              placeholder="Leave your comment here ..."
+            ></textarea>
           </div>
 
           <div className="form-group">
